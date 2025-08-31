438feb5e2a5fd02358073bf1d163cd99b4f25f85
Sanjitha Dilshan <sanjudilshan7233@gmail.com>
2025-08-31 11:55:13 +0530

    State TaskList & set TaskList

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
+import { useState } from "react";
 import { Header } from "./components/Header";
 import { AddTask } from "./components/AddTask";
 import { ShowTask } from "./components/ShowTask";
 import './App.css';
 
+
 function App() {
+
+  const [taskList, setTasklist] = useState([]);
+
   return (
     <div className="App">
       <Header/>
-      <AddTask/>
-      <ShowTask/>
+      <AddTask taskList={taskList} setTaskList={setTasklist}/>
+      <ShowTask taskList={taskList} setTaskList={setTasklist}/>
 
     </div>
   );
--- src/components/ShowTask.js
+++ src/components/ShowTask.js
@@ -1,24 +1,17 @@
-export const ShowTask = () => {
-
-    const tasks = [
-        {id:10001, name: "TASK A", time: "2:09:01 AM 9/14/2030"},
-        {id:10001, name: "TASK B", time: "2:09:01 AM 9/14/2030"},
-        {id:10001, name: "TASK C", time: "2:09:01 AM 9/14/2030"}
-    ]
-
+export const ShowTask = ({taskList, setTaskList}) => {
 
   return (
     <section className="showTask">
         <div className="head">
             <div>
                 <span className="title">Todo</span>
-                <span className="count">0</span>
+                <span className="count">{taskList.length}</span>
             </div>
-            <button className="clearAll">Clear All</button>
+            <button className="clearAll" onClick={() => setTaskList([])}>Clear All</button>
         </div>
         <ul>
-            { tasks.map((task) => (
-                <li>
+            { taskList.map((task) => (
+                <li key={task.id}>
                     <p>
                         <span className="name">{task.name}</span>
                         <span className="time">{task.time}</span>
