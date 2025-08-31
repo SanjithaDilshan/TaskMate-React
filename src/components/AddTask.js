ed62fbbae3d738ba30738c9c4b70babae222ce78
Sanjitha Dilshan <sanjudilshan7233@gmail.com>
2025-08-31 23:52:50 +0530

    Warning Undefined Value

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -9,6 +9,7 @@ export const AddTask = ({taskList, setTaskList, task, setTask}) => {
                 todo.id === task.id ? {id: task.id, name: task.name, time:`${date.toLocaleTimeString()} ${date.toLocaleDateString()}`} : todo 
             ));
             setTaskList(updatedTaskList);
+            setTask({});
         } else {
         const date = new Date();
         const newTask = {
@@ -17,14 +18,14 @@ export const AddTask = ({taskList, setTaskList, task, setTask}) => {
             time: `${date.toLocaleTimeString()} ${date.toLocaleDateString()}`
         }
         setTaskList([...taskList, newTask]);
-        e.target.task.value = "";
+        setTask({});
         }   
     }
 
   return (
     <section className="addTask">
         <form onSubmit={handleSubmit}>
-            <input type="text" name="task" value={task.name} autoComplete="off" placeholder="add task" maxLength={25} onChange={e => setTask({...task, name: e.target.value})} />
+            <input type="text" name="task" value={task.name || ""} autoComplete="off" placeholder="add task" maxLength={25} onChange={e => setTask({...task, name: e.target.value})} />
             <button type="submit">Add</button>
         </form>
     </section>
